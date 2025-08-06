7a5828d908497f0737c7cc11082b29b574a4b00d
curious5091 <curious@ibk.co.kr>
2025-08-06 10:37:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -224,8 +224,7 @@ if run_button:
                 html += '</tr>'
             html += '</table></div>'
 
-            html += '</body></html>'
-            components.html(html, height=1700, scrolling=True, width=1700)
+           st.markdown(html, unsafe_allow_html=True)
 
         except Exception as e:
             st.error("❌ 오류가 발생했습니다.")
